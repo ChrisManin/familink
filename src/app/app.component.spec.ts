8b4af8565c9d55a1275fd5964c9e0884711b5f77
Christophe Manin <cmn@rcfsiege.local>
2019-08-08 14:36:32 +0200

    annulation bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'familink'`, () => {
+  it(`should have as title 'Familink'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('familink');
+    expect(app.title).toEqual('Familink');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('familink');
+    expect(compiled.querySelector('h1').textContent).toContain('Familink');
   });
 });
